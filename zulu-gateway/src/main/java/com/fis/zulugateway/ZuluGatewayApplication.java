444eb7c5a1839d3b3714822441c0b59a86c5c4a6
Anantd2610 <anantdeore2610@gmail.com>
2021-08-16 15:48:13 +0530

    Integrated Eureka with account and loan

---
+++ zulu-gateway/src/main/java/com/fis/zulugateway/ZuluGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.fis.zulugateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+ 
+public class ZuluGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuluGatewayApplication.class, args);
+	}
+
+}
